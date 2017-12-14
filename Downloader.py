4185b72b8b0a337c39a0be67b3f046e7359de9f8
thebatman1 <mrinmay.mukherjee1@gmail.com>
2017-12-15 01:01:46 +0530

    Initial commit

---
+++ Downloader.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import urllib
+import os
+import re
+import sys
+
+base_url = sys.argv[1] ;
+proxies = {'http':'http://172.16.30.20:8080' , 'https':'172.16.30.20:8080'}
+urlfile = urllib.urlopen(base_url , proxies = proxies)
+words = urlfile.read()
+urls =  sorted(set(re.findall(r'[\w\.]+\.480p\.[\w\.]+mkv' , words)))
+i = 0
+for url in urls:
+    print url
+for url in urls:
+    print 'Downloading Episode :', i+1
+    try:
+        urllib.urlretrieve(base_url + url , filename = url)
+    except IOError:
+        print 'Something went wrong!'
+    i = i+1
